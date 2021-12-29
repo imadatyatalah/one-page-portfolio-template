033463394cbce9330df3e47a30411fdcdf8d87f2
imad <imadatyatalah@gmail.com>
2021-12-29 19:48:14 +0100

    Clean up

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,18 @@
 const NextSeo = {
+  defaultTitle: "Imad Atyat-Alah",
+  description:
+    "Self-taught Full Stack JavaScript/TypeScript developer with passion for Front-End.",
+
   openGraph: {
     type: "website",
-    url: "https://next-js-tailwindcss-template.vercel.app/",
-    site_name: "next-js-tailwindcss-template",
+    url: "",
+    site_name: "",
+  },
+
+  twitter: {
+    handle: "@ImadAtyat",
+    site: "@cna-chakra-ui-template",
+    cardType: "summary_large_image",
   },
 };
 
